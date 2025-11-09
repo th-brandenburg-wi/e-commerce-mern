f414aecf31c5e96b3d0c95e71cbfab8f3d0c4fb3
Youssef Benihoud <youssef@ybenihoud.com>
2025-11-09 15:09:54 +0100

    add search feature

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -1,23 +1,27 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import { products } from "../assets/assets";
 
 export const ShopContext = createContext();
 
 const ShopContextProvider = (props) => {
-    
-    const currency = '€';
-    const delivery_fee = 5;
+  const currency = "€";
+  const delivery_fee = 5;
+  const [search, setSearch] = useState("");
+  const [showSearch, setShowSearch] = useState(true);
 
-    
-    const value = {
-        products, currency, delivery_fee
-    }
+  const value = {
+    products,
+    currency,
+    delivery_fee,
+    search,
+    setSearch,
+    showSearch,
+    setShowSearch,
+  };
 
-    return (
-        <ShopContext.Provider value={value}>
-            {props.children}
-        </ShopContext.Provider>
-    )
-}
+  return (
+    <ShopContext.Provider value={value}>{props.children}</ShopContext.Provider>
+  );
+};
 
 export default ShopContextProvider;
