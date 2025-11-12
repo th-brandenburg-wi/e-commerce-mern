48baaee7958449825d482c3d7484bc445aedb99d
Youssef Benihoud <youssef@ybenihoud.com>
2025-11-13 00:44:50 +0100

    fix payment selection

--- frontend/src/pages/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder.jsx
@@ -78,7 +78,10 @@ const PlaceOrder = () => {
           <Title text1={"PAYEMENT"} text2={"METHOD"} />
           {/* PAYEMENT METHOD SELECTION */}
           <div
-            onClick={() => setMethod("stripe")}
+            onClick={() => {
+              console.log("Current" + method);
+              setMethod("stripe");
+            }}
             className="flex gap-3 flex-col lg:flex-row"
           >
             <div className="flex items-center gap-3 border p-2 px-3 cursor-pointer">
@@ -89,19 +92,22 @@ const PlaceOrder = () => {
               ></p>
               <img src={assets.stripe_logo} className="h-5 mx-5" alt="" />
             </div>
-            <div
-              onClick={() => setMethod("cod")}
-              className="flex items-center gap-3 border p-2 px-3 cursor-pointer"
-            >
-              <p
-                className={`min-w-3.5 h-3.5 border rounded-full ${
-                  method === "cod" ? "bg-green-400" : ""
-                }`}
-              ></p>
-              <p className="text-gray-500 text-sm font-medium mx-4">
-                CASH ON DELIVERY
-              </p>
-            </div>
+          </div>
+          <div
+            onClick={() => {
+              console.log("Current" + method);
+              setMethod("cod");
+            }}
+            className="flex items-center gap-3 border p-2 px-3 cursor-pointer"
+          >
+            <p
+              className={`min-w-3.5 h-3.5 border rounded-full ${
+                method === "cod" ? "bg-green-400" : ""
+              }`}
+            ></p>
+            <p className="text-gray-500 text-sm font-medium mx-4">
+              CASH ON DELIVERY
+            </p>
           </div>
           <Button text1="Place Order" clickMethod={() => navigate("/orders")} />
         </div>
