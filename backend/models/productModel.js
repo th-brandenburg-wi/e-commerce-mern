c1b344231d9a6d11b00402581364aaf5d1a79a76
Youssef Benihoud <youssef@ybenihoud.com>
2025-11-13 16:51:07 +0100

    create the first models: user and product

---
+++ backend/models/productModel.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+const productSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  price: { type: Number, required: true },
+  image: { type: Array, required: true },
+  categorie: { type: String, required: true },
+  subcategorie: { type: String, required: true },
+  size: { type: Array, required: true },
+  bestseller: { type: Boolean },
+  date: { type: Number, required: true },
+});
+
+const productModel =
+  mongoose.models.productModel || mongoose.model("product", productSchema);
+
+export default productModel;
